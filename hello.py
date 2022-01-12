46855e5055e44b4728aff81ab6753cba511a2d70
George Seese <gseese@gmail.com>
2022-01-12 00:57:35 -0500

    Added author information

--- hello.py
+++ hello.py
@@ -1,9 +1,25 @@
+################################
+#	Learn Python coding in 2019
+#	
+#	by John Elder
+#
+################################
+
+
 import os
 
 # Clears the bash window
 os.system('clear')
 
-full_name = 'George Seese'
+full_name = 'Samuel Jackson'
+
+age = 41
+
+fav_pizza = {
+	'John': 'pepperoni',
+	'Bob' : 'mushroom',
+	'Mary' : 'cheese'
+}
 
 print(full_name)
 
